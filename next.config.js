24abdaf3e10aadebfa585fc652eeca00378dc4f8
Mark C Allen <mark@markcallen.com>
2022-03-21 09:18:00 -0700

    Fixed merge

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 module.exports = {
   reactStrictMode: true,
-<<<<<<< Updated upstream
-};
-=======
   env: {
     DevCycleKey: 'client-8e6ab66d-a40b-438e-bb17-d7fd5d7eb391',
   },
-}
->>>>>>> Stashed changes
+};
